192cd391275634ecf2a1f15846a84aa4143bc2a4
Brenton Cormick <brentonc@an-a6.tash.21-school.uz>
2025-10-19 18:01:24 +0500

    datetime Asia/Tashkent

--- services/scheduler.py
+++ services/scheduler.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import datetime
+import pytz
 from aiogram import Bot
 from services.weather_service import get_weather
 from utils.state import user_notifications, user_cities, user_languages
@@ -7,13 +8,17 @@ from utils.state import user_notifications, user_cities, user_languages
 
 async def send_daily_forecasts(bot: Bot):
     """Проверяет пользователей и отправляет прогноз, если наступило их время"""
-    now = datetime.now()
-    current_hour = now.hour
+    # используем время Ташкента
+    tashkent_tz = pytz.timezone("Asia/Tashkent")
+    now_utc = datetime.utcnow().replace(tzinfo=pytz.utc)
+    now_local = now_utc.astimezone(tashkent_tz)
+    current_hour = now_local.hour
 
     for user_id, notif in user_notifications.items():
         if not notif.get("enabled"):
             continue
 
+        # Проверяем, совпадает ли локальное время с временем пользователя
         if notif.get("hour") == current_hour:
             city = user_cities.get(user_id)
             lang = user_languages.get(user_id, "ru")
